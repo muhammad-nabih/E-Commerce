e7937cf1184b12c247e9c34674f0435a7b95fdcf
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-04-17 12:15:09 +0200

    link electro marekt to the main page

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -84,7 +84,7 @@ const Footer = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2024{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
+            <a href="/" className="hover:underline">
               إلكتروماركت{" "}
             </a>
             جميع الحقوق محفوظة
