8573ef58aea1c99afcec85be2644aef226f0a3a6
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-04-17 12:11:21 +0200

    make footer  style

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,64 +1,135 @@
-"use client";
+import { FaDiscord, FaFacebook, FaGithub, FaTwitter } from "react-icons/fa";
 
-import { Footer } from "flowbite-react";
-import {
-  BsDribbble,
-  BsFacebook,
-  BsGithub,
-  BsInstagram,
-  BsTwitter,
-} from "react-icons/bs";
-
-export default function Footer_() {
+const Footer = () => {
   return (
-    <Footer container className="bg-gray-950 rounded-none ">
-      <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div style={{ color: "white !important" }}>
-            <Footer.Brand
-              href="/"
-              src="/ecommerce.svg"
-              alt="logo ecommerce "
-              name="إلكتروماركت"
-            />
+    <footer className="bg-white dark:bg-gray-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="/" className="flex items-center">
+              <img
+                src="/ecommerce.svg"
+                className="h-8 me-3"
+                alt="FlowBite Logo"
+              />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                إلكتروماركت
+              </span>
+            </a>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6 text-center ">
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <Footer.Title className="text-zinc-200" title="ماذا عنا " />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">محمد </Footer.Link>
-                <Footer.Link href="#">زياد</Footer.Link>
-              </Footer.LinkGroup>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                المصادر
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="https://flowbite.com/" className="hover:underline">
+                    Flowbite
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindcss.com/"
+                    className="hover:underline"
+                  >
+                    Tailwind CSS
+                  </a>
+                </li>
+              </ul>
             </div>
             <div>
-              <Footer.Title className="text-zinc-200" title="Follow us" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Github</Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                تابعنا
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a
+                    href="https://github.com/themesberg/flowbite"
+                    className="hover:underline "
+                  >
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://discord.gg/4eeurUVvTy"
+                    className="hover:underline"
+                  >
+                    Discord
+                  </a>
+                </li>
+              </ul>
             </div>
             <div>
-              <Footer.Title className="text-zinc-200" title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                الخصوصية
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-
-        <Footer.Divider />
-        <div className="w-full  sm:flex sm:items-center sm:justify-between ">
-          <Footer.Copyright href="#" by="إلكتروماركت" year={2024} />
-          <div className="mt-4  gap-3 flex  sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <a href="https://flowbite.com/" className="hover:underline">
+              إلكتروماركت{" "}
+            </a>
+            جميع الحقوق محفوظة
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+            >
+              <FaFacebook />
+              <span className="sr-only">Facebook page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+             <FaDiscord/>
+              <span className="sr-only">Discord community</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <FaTwitter/>
+              <span className="sr-only">Twitter page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <FaGithub />
+              <span className="sr-only">GitHub account</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <img src="/ecommerce.svg" alt="" />
+              <span className="sr-only">Dribbble account</span>
+            </a>
           </div>
         </div>
       </div>
-    </Footer>
+    </footer>
   );
-}
+};
+
+export default Footer;
